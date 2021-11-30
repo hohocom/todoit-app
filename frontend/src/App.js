21c314b111ecb1a845505be6a74497049b619a7d
mihyeon <2_5_3_1@naver.com>
2021-11-30 11:23:45 +0900

    frontend: update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,28 @@ import React from 'react'
 import { useRoutes } from 'react-router'
 import JoinPage from './pages/JoinPage'
 import LoginPage from './pages/LoginPage'
+import MainPage from './pages/MainPage'
 import WelcomePage from './pages/WelcomePage'
+import WorkspaceCreatePage from './pages/WorkspaceCreatePage'
 
 function App() {
   const route = useRoutes([
-    { path: '/', element: <WelcomePage /> },
+    { path: '', element: <WelcomePage /> },
     {
-      path: '/login',
+      path: 'login',
       element: <LoginPage />,
-      // children: [{ path: ':id', element: <Main /> }],
     },
     {
-      path: '/join',
+      path: 'join',
       element: <JoinPage />,
+    },
+    {
+      path: 'workspaces',
+      element: <WorkspaceCreatePage />,
+      children: [
+        { path: ':id', element: <MainPage /> },
+        { path: 'temp', element: <WorkspaceCreatePage /> },
+      ],
     },
   ])
 
---
+++ frontend/src/pages/WorkspaceCreatePage.js
@@ -0,0 +1,5 @@
+function WorkspaceCreatePage() {
+  return <div>작업공간 생성 페이지</div>
+}
+
+export default WorkspaceCreatePage
