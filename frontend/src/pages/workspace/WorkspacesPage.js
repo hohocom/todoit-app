1d6cee2d64546e0879767abb5f899b5082526719
gojaebeom <const.gjb@gmail.com>
2021-12-06 10:30:20 +0900

    frontend: update

--- frontend/src/pages/workspace/WorkspacesPage.js
+++ frontend/src/pages/workspace/WorkspacesPage.js
@@ -14,8 +14,41 @@ function WorkspacesPage() {
         <div className="font-shadow2">환영합니다!</div>
       </div>
       <div className="w-full sm:w-[400px] h-full sm:h-[500px] bg-[#F2F2F2] rounded-t-3xl rounded-b-none sm:rounded-xl flex flex-col justify-start items-center p-[30px] z-10">
-        <h2>고재범</h2>
-        <h3>님의 워크스페이스</h3>
+        <div className="w-full">
+          <h2>가입된 워크스페이스 ( 2 )</h2>
+          <div className="border rounded-[4px] flex justify-between items-center bg-gray-50 mt-2">
+            <div
+              id="title"
+              className="p-4 cursor-pointer hover:text-yellow-500 font-apple-bold"
+            >
+              호호컴퍼니
+            </div>
+            <div className="p-4">
+              <i className="mx-2 cursor-pointer far fa-edit hover:text-yellow-500"></i>
+              <i className="mx-2 cursor-pointer far fa-trash-alt hover:text-red-500"></i>
+            </div>
+          </div>
+          <div className="border rounded-[4px] flex justify-between items-center bg-gray-50 mt-2">
+            <div
+              id="title"
+              className="p-4 cursor-pointer hover:text-yellow-500 font-apple-bold"
+            >
+              방송대학과일정
+            </div>
+            <div className="p-4">
+              <i className="mx-2 cursor-pointer far fa-edit hover:text-yellow-500"></i>
+              <i className="mx-2 cursor-pointer far fa-trash-alt hover:text-red-500"></i>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full mt-4">
+          <h2>초대코드로 가입</h2>
+          <input
+            className="w-full p-4 border border-[#FFC35E] rounded-[4px] outline-none"
+            placeholder="초대코드를 입력해주세요."
+          />
+        </div>
       </div>
       <div
         id="particles-js"
