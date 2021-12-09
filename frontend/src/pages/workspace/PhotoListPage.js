7fb45fd2ff244d7ca7af7d222147ab906ead87e6
gojaebeom <const.gjb@gmail.com>
2021-12-09 13:00:26 +0900

    frontend:소캣통신연습

--- frontend/src/pages/workspace/PhotoListPage.js
+++ frontend/src/pages/workspace/PhotoListPage.js
@@ -1,16 +1,37 @@
 import WorkspaceLayout from '../../layouts/WorkspaceLayout'
-import SockJS from 'sockjs-client';
-import StompJs from '@stomp/stompjs';
-import { useEffect, useRef } from 'react'
+import SockJsClient from 'react-stomp'
+import { useRef, useState } from 'react'
 
 function PhotoListPage() {
-  useEffect(() => {
-    // new StompJs.
-  }, [])
+  const websocket = useRef()
+
+  const [texts, setTexts] = useState([])
+
+  const handleClickSendTo = () => {
+    const data = {
+      text: 'hellow',
+    }
+    websocket.current.sendMessage('/sendTo', JSON.stringify(data))
+  }
 
   return (
     <WorkspaceLayout>
-
+      <SockJsClient
+        url={`${process.env.REACT_APP_API_URL}/start`}
+        topics={['/topics/sendTo', '/topics/template', '/topics/api']}
+        onMessage={(msg) => {
+          console.log(msg)
+          setTexts(texts.concat(msg.text))
+        }}
+        ref={websocket}
+      />
+      <button onClick={handleClickSendTo}>SendTo</button>
+      <div>
+        {texts.map((text, index) => {
+          console.log(text)
+          return <p key={index}>{text}</p>
+        })}
+      </div>
     </WorkspaceLayout>
   )
 }
