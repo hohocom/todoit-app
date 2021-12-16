20308dbcc71a3bf4d1ca39fe53d21a84379585f1
gojaebeom <const.gjb@gmail.com>
2021-12-16 11:44:00 +0900

    update

--- frontend/src/components/shared/AvatarGroup.js
+++ frontend/src/components/shared/AvatarGroup.js
@@ -6,13 +6,13 @@ function AvatarGroup({ items, space = "m" }) {
   useEffect(() => {}, []);
 
   return (
-    <div className="flex">
+    <div className="flex relative">
       <div
         className={`flex
-    ${space === "s" && "-space-x-6"}
-    ${space === "m" && "-space-x-4"}
-    ${space === "l" && "-space-x-2"}
-    `}
+          ${space === "s" && "-space-x-6"}
+          ${space === "m" && "-space-x-4"}
+          ${space === "l" && "-space-x-2"}
+        `}
       >
         {items.length >= 1 &&
           items.map((user, index) => {
@@ -30,10 +30,34 @@ function AvatarGroup({ items, space = "m" }) {
           })}
       </div>
       {items.length >= 4 && (
-        <button className="hover:text-[#FF9E5D] mt-1 ml-1">
+        <button
+          className="hover:text-[#FF9E5D] mt-1 ml-1"
+          onClick={() => {
+            document.querySelector(
+              "#avatarGroupModalBackground"
+            ).style.display = "block";
+            document.querySelector("#avatarGroupModal").style.display = "block";
+          }}
+        >
           +{items.length - 3}명
         </button>
       )}
+      <div
+        id="avatarGroupModalBackground"
+        className="fixed left-0 top-0 w-full h-full z-40"
+        onClick={(e) => {
+          const backgroundEl = e.target;
+          backgroundEl.style.display = "none";
+          const seletorEl = backgroundEl.nextSibling;
+          seletorEl.style.display = "none";
+        }}
+      ></div>
+      <div
+        id="avatarGroupModal"
+        className="absolute -bottom-10 min-w-[120px] p-2 bg-white rounded-md z-50 border"
+      >
+        hello world!
+      </div>
     </div>
   );
 }
