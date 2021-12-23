5bf4a0efda7630cc6e92be112dd29110ce2b5af8
kimmihyeon12 <2_5_3_1@naver.com>
2021-12-23 17:34:38 +0900

    frontend: profile ui update

--- frontend/src/components/common/Avatar.js
+++ frontend/src/components/common/Avatar.js
@@ -1,9 +1,9 @@
 import nullProfileImg from "assets/images/null-profile.svg";
-
+import noneImg from "assets/images/noneImg.png";
 function Avatar({ thumbnailImage, size = 40, hover = true }) {
   return (
     <img
-      src={thumbnailImage ? process.env.REACT_APP_API_URL+"/images"+thumbnailImage : nullProfileImg}
+      src={thumbnailImage ? process.env.REACT_APP_API_URL+"/images"+thumbnailImage : noneImg}
       alt="avatar"
       className={"inline-block object-cover transition duration-150 ease-in-out bg-gray-300 rounded-full cursor-pointer".concat(
         hover === true && " ring-2 ring-white hover:ring-[#fabb92]"
